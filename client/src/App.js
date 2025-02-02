d1510ed506428eb71a88316cddf7efbb14ac8e0a
ArDavid14 <dcubillos904@gmail.com>
2025-02-02 16:45:44 -0500

    form

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="Datos">
+        <label>Codigo: <input type="text"></input></label><br/>
+        <label>Nombre: <input type="text"></input></label><br/>
+        <label>Apellido: <input type="text"></input></label><br/>
+        <label>Celular: <input type="text"></input></label><br/>
+        <label>Dirección: <input type="text"></input></label><br/>
+      </div>
     </div>
   );
 }
