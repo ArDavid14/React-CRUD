c403f70e57faa8c992a4b1e760c7c683ad2e0a77
Santiagocsie <santiagocabrasierra@gmail.com>
2025-02-02 18:10:24 -0500

    creacion de formulario basico

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,47 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [codigo, setCodigo] = useState(0); 
+  const [nombre, setNombre] = useState(""); 
+  const [apellido, setApellido] = useState(""); 
+  const [celular, setCelular] = useState(0); 
+  const [direccion, setDireccion] = useState(""); 
+
+  const mostratDatos = ()=>{
+    alert(nombre)
+  }
+
   return (
     <div className="App">
-      <div className="Datos">
-        <label>Codigo: <input type="text"></input></label><br/>
-        <label>Nombre: <input type="text"></input></label><br/>
-        <label>Apellido: <input type="text"></input></label><br/>
-        <label>Celular: <input type="text"></input></label><br/>
-        <label>Dirección: <input type="text"></input></label><br/>
-        <button>Registrar</button>
+      <div className="datos">
+        <label>Codigo: <input
+        onChange={(event)=>{
+          setCodigo(event.target.value);
+        }} 
+        type="number"></input></label><br/>
+        <label>Nombre: <input 
+        onChange={(event)=>{
+          setNombre(event.target.value);
+        }} 
+        type="text"></input></label><br/>
+        <label>Apellido: <input
+        onChange={(event)=>{
+          setApellido(event.target.value);
+        }} 
+        type="text"></input></label><br/>
+        <label>Celular: <input
+        onChange={(event)=>{
+          setCelular(event.target.value);
+        }} 
+        type="number"></input></label><br/>
+        <label>Dirección: <input
+        onChange={(event)=>{
+          setDireccion(event.target.value);
+        }} 
+        type="text"></input></label><br/>
+        <button onClick={mostratDatos}>Registrar</button>
       </div>
     </div>
   );
